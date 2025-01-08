73ba68939fbc1d7d75e657a61dea029eeb23d3d4
Tchoubi11 <tchoubayefranck@gmail.com>
2025-01-09 00:12:46 +0100

    Ajout des methodes pour les routes contacts,habitats,login et services.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,6 +8,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+/**
+ * page d'accueil
+ * @return Response
+ * 
+ */
+
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
@@ -24,10 +30,9 @@ class HomeController extends AbstractController
             'Marais' => ['Couguar', 'Renard gris']
         ];
 
-       
         $services = [
             'Restauration',
-            'Visite du Zoo en petit train',
+            'Visite du zoo en petit train',
             'Visites des habitats avec un guide (gratuit)'
         ];
        
@@ -39,5 +44,61 @@ class HomeController extends AbstractController
             
         ]);
     }
-}
 
+    
+     #[Route("/services", name:"app_services")]
+     
+    public function services(): Response
+    {
+       
+        $services = [
+            'Restauration'=>'Dégustez une variété de plats savoureux dans nos restaurants situés au cœur du zoo. Nous proposons des options pour toute la famille, y compris des repas végétariens et adaptés aux enfants.',
+            'Visite du zoo en petit train'=>'Explorez tout le zoo sans effort grâce à notre petit train. Idéal pour les familles et les visiteurs souhaitant profiter d\'une vue d\'ensemble du parc tout en se relaxant. ',
+            'Visites des habitats avec un guide (gratuit)'=>'Plongez dans l\'univers fascinant des animaux grâce à nos visites guidées gratuites. Découvrez les secrets des habitats de la savane, de la jungle et des marais .'
+        ];
+        $names=['Restauration','Visite du Zoo en petit train','Visites des habitats avec un guide (gratuit)'];
+        $descriptions=['Dégustez une variété de plats savoureux dans nos restaurants situés au cœur du zoo. Nous proposons des options pour toute la famille, y compris des repas végétariens et adaptés aux enfants.','Explorez tout le zoo sans effort grâce à notre petit train. Idéal pour les familles et les visiteurs souhaitant profiter d\'une vue d\'ensemble du parc tout en se relaxant. ','Plongez dans l\'univers fascinant des animaux grâce à nos visites guidées gratuites. Découvrez les secrets des habitats de la savane, de la jungle et des marais .'];
+
+        return $this->render('home/services.html.twig',[
+         'services'=>$services,
+         'names'=>$names,
+         'descriptions'=>$descriptions,
+        ]);
+
+    }
+
+    #[Route("/habitats",name:"app_habitats")]
+    public function Habitats():Response
+    {
+        $habitats = [
+            'Savane' => ['Lions', 'Girafes', 'Zèbres'],
+            'Jungle' => ['Singes', 'Tigres', 'Léopards'],
+            'Marais' => ['Couguar', 'Renard gris']
+        ];
+        return $this->render('home/habitats.html.twig',[
+        'habitats' => $habitats,
+        ]);
+    }
+
+    #[Route("/contacts",name:"app_contact")]
+    public function Contact():Response
+    {
+        $contacts = [
+            ['name' => 'Jean-Marie', 'email' => 'jean.marie@example.com', 'phone' => '123-456-7890','mission'=>'Intendant'],
+            ['name' => 'Frank Herve', 'email' => 'frank.herve@example.com', 'phone' => '987-654-3210','mission'=>'Cordonnateur'],
+            ['name' => 'Albert Einstein', 'email' => 'albert.einstein@example.com', 'phone' => '456-789-1234','mission'=>'Directeur '],
+        ];
+        return $this->render('home/contact.html.twig',[
+           'contacts' => $contacts,
+        ]);
+    }
+ 
+    #[Route( "/login",name:"app_login")]
+   public function Login():Response
+   {
+    return $this->render('home/login.html.twig',[
+    
+    ]);
+   }
+  
+}
