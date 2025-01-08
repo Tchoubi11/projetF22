08c493139c7752a5e254597cd3cc1904d8d77352
Tchoubi11 <tchoubayefranck@gmail.com>
2025-01-08 23:17:14 +0100

    Ajout des variables reviews,habitats et services dans le HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,18 +1,42 @@
 <?php
 
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $reviews = [
+            "Très belle expérience, mes enfants ont adoré voir les tigres!" => "Cathérine",
+            "Un endroit magnifique et bien entretenu. Bravo à l'équipe!" => "Marcus",
+            "Les habitats sont vraiment bien conçus. Un moment inoubliable." => "John",
+        ];
+
+        $habitats = [
+            'Savane' => ['Lions', 'Girafes', 'Zèbres'],
+            'Jungle' => ['Singes', 'Tigres', 'Léopards'],
+            'Marais' => ['Couguar', 'Renard gris']
+        ];
+
+       
+        $services = [
+            'Restauration',
+            'Visite du Zoo en petit train',
+            'Visites des habitats avec un guide (gratuit)'
+        ];
+       
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'reviews' => $reviews,
+            'habitats' => $habitats,
+            'services' => $services,
         ]);
     }
 }
+
