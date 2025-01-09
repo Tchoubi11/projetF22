39894e3589802b955910b76f3ab4b1f8cccdefdd
Tchoubi11 <tchoubayefranck@gmail.com>
2025-01-09 09:27:25 +0100

    modification du fichier Image.html.twig

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -22,7 +23,7 @@ class ImageType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
+           'data_class' => Image::class,  
         ]);
     }
 }
