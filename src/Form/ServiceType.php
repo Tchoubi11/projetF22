a794f6638990431ff007c2d53323d789e7da761f
Tchoubi11 <tchoubayefranck@gmail.com>
2025-01-13 22:02:21 +0100

    Modification du fichier ServiceType.php

--- src/Form/ServiceType.php
+++ src/Form/ServiceType.php
@@ -6,15 +6,24 @@ use App\Entity\Service;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ServiceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('description')
-        ;
+        ->add('name', TextType::class, [
+            'label' => 'Nom du service',
+            'required' => true,
+            'attr' => ['placeholder' => 'Entrez le nom du service'],
+        ])
+        ->add('description', TextareaType::class, [
+            'label' => 'Description du service',
+            'required' => true,
+            'attr' => ['placeholder' => 'Entrez une description pour le service'],
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
