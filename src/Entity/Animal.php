bbac93ccbb3dad0df01e74fd5325ef92f6352783
Tchoubi11 <tchoubayefranck@gmail.com>
2025-01-16 14:35:46 +0100

    Modifications des entités Habitat et Animal

--- src/Entity/Animal.php
+++ src/Entity/Animal.php
@@ -13,18 +13,28 @@ class Animal
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 50)]
-    private ?string $prenom = null;
+    #[ORM\Column(length: 255)]
+    private string $prenom;
+
+    #[ORM\Column(length: 255)]
+    private string $race;
+
+    #[ORM\Column(length: 255)]
+    private string $image;
 
     #[ORM\Column(length: 50)]
     private ?string $etat = null;
+
+    #[ORM\ManyToOne(targetEntity: Habitat::class, inversedBy: 'animaux')]
+    #[ORM\JoinColumn(nullable: true)]  // Permettre la nullabilité et discocier un animal de son habitat si nécéssaire
+    private ?Habitat $habitat = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getPrenom(): ?string
+    public function getPrenom(): string
     {
         return $this->prenom;
     }
@@ -32,6 +42,30 @@ class Animal
     public function setPrenom(string $prenom): static
     {
         $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getRace(): string
+    {
+        return $this->race;
+    }
+
+    public function setRace(string $race): static
+    {
+        $this->race = $race;
+
+        return $this;
+    }
+
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
 
         return $this;
     }
@@ -46,5 +80,30 @@ class Animal
         $this->etat = $etat;
 
         return $this;
+    }
+
+    public function getHabitat(): ?Habitat
+    {
+        return $this->habitat;
+    }
+
+    public function setHabitat(?Habitat $habitat): static
+    {
+        $this->habitat = $habitat;
+
+        return $this;
+    }
+
+    /**
+     * Renvoie une chaîne descriptive de l'animal.
+     */
+    public function __toString(): string
+    {
+        return sprintf(
+            '%s (%s) - État: %s',
+            $this->prenom,
+            $this->race,
+            $this->etat ?? 'N/A'
+        );
     }
 }
--- src/Entity/Habitat.php
+++ src/Entity/Habitat.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\HabitatRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: HabitatRepository::class)]
@@ -10,17 +12,28 @@ class Habitat
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(type: 'string', length: 255)]
     private ?string $nom = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(type: 'text')]
     private ?string $description = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(type: 'text', nullable: true)]
     private ?string $commentaire_habitat = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $image = null;
+
+    #[ORM\OneToMany(mappedBy: 'habitat', targetEntity: Animal::class, cascade: ['persist', 'remove'])]
+    private Collection $animaux;
+
+    public function __construct()
+    {
+        $this->animaux = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -32,10 +45,9 @@ class Habitat
         return $this->nom;
     }
 
-    public function setNom(string $nom): static
+    public function setNom(string $nom): self
     {
         $this->nom = $nom;
-
         return $this;
     }
 
@@ -44,10 +56,9 @@ class Habitat
         return $this->description;
     }
 
-    public function setDescription(string $description): static
+    public function setDescription(string $description): self
     {
         $this->description = $description;
-
         return $this;
     }
 
@@ -56,10 +67,57 @@ class Habitat
         return $this->commentaire_habitat;
     }
 
-    public function setCommentaireHabitat(string $commentaire_habitat): static
+    public function setCommentaireHabitat(?string $commentaire_habitat): self
     {
         $this->commentaire_habitat = $commentaire_habitat;
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    /**
+     * @return Collection|Animal[]
+     */
+    public function getAnimaux(): Collection
+    {
+        return $this->animaux;
+    }
+
+    public function addAnimal(Animal $animal): self
+    {
+        if (!$this->animaux->contains($animal)) {
+            $this->animaux->add($animal);
+
+            // Mettez à jour l'association sur l'autre côté
+            $animal->setHabitat($this);
+        }
 
         return $this;
+    }
+
+    public function removeAnimal(Animal $animal): self
+    {
+        if ($this->animaux->removeElement($animal)) {
+            // Désassociez l'habitat de l'animal si nécessaire
+            if ($animal->getHabitat() === $this) {
+                $animal->setHabitat(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->nom ?? 'Nom indisponible';
     }
 }
