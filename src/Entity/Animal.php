b16f0eaf87cc1bd6254dd49d2ad9639abe6b1b2d
Tchoubi11 <tchoubayefranck@gmail.com>
2025-01-19 16:37:11 +0100

    Modification AnimalRepository.php et Animal.php

--- src/Entity/Animal.php
+++ src/Entity/Animal.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\AnimalRepository;
+
 
 #[ORM\Entity(repositoryClass: AnimalRepository::class)]
 class Animal
--- src/Repository/AnimalRepository.php
+++ src/Repository/AnimalRepository.php
@@ -7,7 +7,10 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Animal>
+ * @method Animal|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Animal|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Animal[]    findAll()
+ * @method Animal[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class AnimalRepository extends ServiceEntityRepository
 {
@@ -16,28 +19,13 @@ class AnimalRepository extends ServiceEntityRepository
         parent::__construct($registry, Animal::class);
     }
 
-    //    /**
-    //     * @return Animal[] Returns an array of Animal objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('a.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Animal
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    // Exemple de méthode personnalisée pour rechercher des animaux par état
+    public function findByEtat($etat)
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.etat = :etat')
+            ->setParameter('etat', $etat)
+            ->getQuery()
+            ->getResult();
+    }
 }
