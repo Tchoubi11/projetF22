45c3a656d9a3f3f6536a277b790764d82c46f31d
Tchoubi11 <tchoubayefranck@gmail.com>
2025-01-21 23:39:38 +0100

    Modification RapportVeterinaireType

--- src/Form/RapportVeterinaireType.php
+++ src/Form/RapportVeterinaireType.php
@@ -2,7 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\RapportVeterinaire;
+use App\Entity\Animal;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,14 +16,25 @@ class RapportVeterinaireType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
-        ;
+            ->add('animal', EntityType::class, [
+                'class' => Animal::class,
+                'choice_label' => 'prenom', 
+                'label' => 'Animal',
+            ])
+            ->add('date', DateTimeType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date du rapport',
+            ])
+            ->add('etat', TextareaType::class, [
+                'label' => 'État du rapport',
+                'required' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => RapportVeterinaire::class,
         ]);
     }
 }
