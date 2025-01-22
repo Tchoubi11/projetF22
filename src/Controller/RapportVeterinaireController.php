8b2c8f960048402ee66297a61c2e9feacbf22c38
Tchoubi11 <tchoubayefranck@gmail.com>
2025-01-22 10:53:46 +0100

    Mise à jour RapportVeterinaieController

--- src/Controller/RapportVeterinaireController.php
+++ src/Controller/RapportVeterinaireController.php
@@ -9,6 +9,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use App\Entity\RapportVeterinaire;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
 
 #[Route('/admin/user')]
 class RapportVeterinaireController extends AbstractController
@@ -40,7 +46,7 @@ class RapportVeterinaireController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             // Vérifie à nouveau si le rôle admin est assigné dans les données soumises
             if (in_array('ROLE_ADMIN', $user->getRoles())) {
-                throw $this->createAccessDeniedException('Impossible d’attribuer le rôle Administrateur.');
+                throw $this->createAccessDeniedException('Impossible d\'attribuer le rôle Administrateur.');
             }
 
             $em->persist($user);
@@ -67,4 +73,77 @@ class RapportVeterinaireController extends AbstractController
             'users' => $users,
         ]);
     }
+
+     // Liste des rapports vétérinaires
+     #[Route('/', name: 'rapport_veterinaire_list', methods: ['GET'])]
+     public function list(EntityManagerInterface $em): Response
+     {
+         $rapports = $em->getRepository(RapportVeterinaire::class)->findAll();
+ 
+         return $this->render('admin/rapport_veterinaire/list.html.twig', [
+             'rapports' => $rapports,
+         ]);
+     }
+ 
+     // Créer un nouveau rapport vétérinaire
+     #[Route('/new', name: 'rapport_veterinaire_new', methods: ['GET', 'POST'])]
+     public function new(Request $request, EntityManagerInterface $em): Response
+     {
+         $rapport = new RapportVeterinaire();
+ 
+         $form = $this->createFormBuilder($rapport)
+             ->add('date', DateType::class, [
+                 'widget' => 'single_text',
+                 'label' => 'Date du rapport',
+             ])
+             ->add('detail', TextareaType::class, [
+                 'label' => 'Detail',
+             ])
+             ->add('animal', null, [
+                 'choice_label' => 'prenom',
+                 'label' => 'Animal',
+             ])
+             ->add('save', SubmitType::class, [
+                 'label' => 'Enregistrer le rapport',
+             ])
+             ->getForm();
+ 
+         $form->handleRequest($request);
+ 
+         if ($form->isSubmitted() && $form->isValid()) {
+             $em->persist($rapport);
+             $em->flush();
+ 
+             $this->addFlash('success', 'Rapport vétérinaire créé avec succès.');
+ 
+             return $this->redirectToRoute('rapport_veterinaire_list');
+         }
+ 
+         return $this->render('admin/rapport_veterinaire/new.html.twig', [
+             'form' => $form->createView(),
+         ]);
+     }
+ 
+     // Afficher les détails d'un rapport vétérinaire
+     #[Route('/{id}', name: 'rapport_veterinaire_show', methods: ['GET'])]
+     public function show(RapportVeterinaire $rapport): Response
+     {
+         return $this->render('admin/rapport_veterinaire/show.html.twig', [
+             'rapport' => $rapport,
+         ]);
+     }
+ 
+     // Supprimer un rapport vétérinaire
+     #[Route('/delete/{id}', name: 'rapport_veterinaire_delete', methods: ['POST'])]
+     public function delete(Request $request, RapportVeterinaire $rapport, EntityManagerInterface $em): Response
+     {
+         if ($this->isCsrfTokenValid('delete' . $rapport->getId(), $request->request->get('_token'))) {
+             $em->remove($rapport);
+             $em->flush();
+ 
+             $this->addFlash('success', 'Rapport vétérinaire supprimé avec succès.');
+         }
+ 
+         return $this->redirectToRoute('rapport_veterinaire_list');
+     }
 }
