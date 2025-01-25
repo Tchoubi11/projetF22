58702f258d9fc2a1fc5836b9b3972965698f9e5e
Tchoubi11 <tchoubayefranck@gmail.com>
2025-01-25 09:10:23 +0100

    Mise à jour AlimentationController.php

--- src/Controller/AlimentationController.php
+++ src/Controller/AlimentationController.php
@@ -1,18 +1,52 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Employe;
 
+use App\Entity\Alimentation;
+use App\Form\AlimentationType;
+use App\Repository\AlimentationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
-final class AlimentationController extends AbstractController
+#[Route('/employe/alimentation')]
+class AlimentationController extends AbstractController
 {
-    #[Route('/alimentation', name: 'app_alimentation')]
-    public function index(): Response
+    private ManagerRegistry $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
     {
-        return $this->render('alimentation/index.html.twig', [
-            'controller_name' => 'AlimentationController',
+        $this->doctrine = $doctrine;
+    }
+
+    #[Route('/', name: 'employe_alimentation_index', methods: ['GET'])]
+    public function index(AlimentationRepository $alimentationRepository): Response
+    {
+        return $this->render('employe/alimentation/index.html.twig', [
+            'alimentations' => $alimentationRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'employe_alimentation_new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $alimentation = new Alimentation();
+        $form = $this->createForm(AlimentationType::class, $alimentation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->doctrine->getManager();
+            $entityManager->persist($alimentation);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Consommation ajoutée avec succès.');
+            return $this->redirectToRoute('employe_alimentation_index');
+        }
+
+        return $this->render('employe/alimentation/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
