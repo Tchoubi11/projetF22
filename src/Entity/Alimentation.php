02f2318ef023402ee00ef4c00433ea32ae271f76
Tchoubi11 <tchoubayefranck@gmail.com>
2025-01-25 08:12:37 +0100

    Création entité Alimentation

---
+++ src/Entity/Alimentation.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AlimentationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AlimentationRepository::class)]
+class Alimentation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTimeInterface $dateHeure = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nourriture = null;
+
+    #[ORM\Column]
+    private ?float $quantite = null;
+
+    #[ORM\ManyToOne(targetEntity: Animal::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Animal $animal = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateHeure(): ?\DateTimeInterface
+    {
+        return $this->dateHeure;
+    }
+
+    public function setDateHeure(\DateTimeInterface $dateHeure): static
+    {
+        $this->dateHeure = $dateHeure;
+
+        return $this;
+    }
+
+    public function getNourriture(): ?string
+    {
+        return $this->nourriture;
+    }
+
+    public function setNourriture(string $nourriture): static
+    {
+        $this->nourriture = $nourriture;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?float
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(float $quantite): static
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    public function getAnimal(): ?Animal
+    {
+        return $this->animal;
+    }
+
+    public function setAnimal(Animal $animal): static
+    {
+        $this->animal = $animal;
+
+        return $this;
+    }
+}
