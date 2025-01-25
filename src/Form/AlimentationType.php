14bde8289f67ae5c31cb0bb6e34568f47260a2b1
Tchoubi11 <tchoubayefranck@gmail.com>
2025-01-25 09:04:38 +0100

    Mise à jour AlimentationType.php

--- src/Form/AlimentationType.php
+++ src/Form/AlimentationType.php
@@ -21,12 +21,11 @@ class AlimentationType extends AbstractType
                 'widget' => 'single_text',
                 'label' => 'Date et heure',
             ])
+            ->add('nourriture', TextType::class, [
+                'label' => 'Type de nourriture',
+            ])  
             ->add('grammage', NumberType::class, [
                 'label' => 'Grammage (en grammes)',
-            ])
-            
-            ->add('quantite', NumberType::class, [
-                'label' => 'Quantité (en kg)',
             ])
             ->add('animal', EntityType::class, [
                 'class' => Animal::class,
