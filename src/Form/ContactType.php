25e57282e01c866db87213df3fa6d0df96648dab
Tchoubi11 <tchoubayefranck@gmail.com>
2025-01-29 10:21:04 +0100

    Cr&ation et mise à jour de ContactType.php

---
+++ src/Form/ContactType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ContactRequest;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre de la demande',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description de la demande',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre Email',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => ContactRequest::class,
+        ]);
+    }
+}
