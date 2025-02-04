a103279b6cb4f68cda34f19f03e6dddd673e17d8
Tchoubi11 <tchoubayefranck@gmail.com>
2025-02-04 10:34:04 +0100

    Mise à jour UserController.php,HomeController.php et security.yaml

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,21 +6,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use App\Entity\Avis;
 use App\Entity\Habitat;
-use App\Entity\User;
 use App\Form\AvisType;
 use App\Repository\AvisRepository;
 use App\Repository\HabitatRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse; 
 
 
@@ -29,22 +19,13 @@ class HomeController extends AbstractController
 {
     private $habitatRepository;
     private $avisRepository;
-    private $entityManager;
-    private $mailer;
-    private $userRepository;
-
+    
     public function __construct(
         HabitatRepository $habitatRepository,
-        AvisRepository $avisRepository,
-        EntityManagerInterface $entityManager,
-        MailerInterface $mailer,  
-        UserRepository $userRepository  
+        AvisRepository $avisRepository,   
     ) {
         $this->habitatRepository = $habitatRepository;
         $this->avisRepository = $avisRepository;
-        $this->entityManager = $entityManager;
-        $this->mailer = $mailer;
-        $this->userRepository = $userRepository;
     }
     
     #[Route('/', name: 'app_home')]
@@ -80,10 +61,10 @@ class HomeController extends AbstractController
         ];
 
        $services = [
-    ['name' => 'Restauration', 'icon' => 'fas fa-utensils'],
-    ['name' => 'Visite du zoo en petit train', 'icon' => 'fas fa-train'],
-    ['name' => 'Visites des habitats avec un guide (gratuit)', 'icon' => 'fas fa-users']
-];
+         ['name' => 'Restauration', 'icon' => 'fas fa-utensils'],
+         ['name' => 'Visite du zoo en petit train', 'icon' => 'fas fa-train'],
+         ['name' => 'Visites des habitats avec un guide (gratuit)', 'icon' => 'fas fa-users']
+      ];
 
 
         return $this->render('home/index.html.twig', [
@@ -270,164 +251,15 @@ class HomeController extends AbstractController
     }
 
     
-    #[Route('/admin/user/create', name: 'admin_user_create')]
-    public function createUser(Request $request, UserPasswordHasherInterface $passwordHasher): Response 
-    {
-        $user = new User();
-    
-        $form = $this->createFormBuilder($user)
-            ->add('username', EmailType::class, ['required' => false])
-            ->add('password', PasswordType::class)
-            ->add('roles', ChoiceType::class, [
-                'choices' => [
-                    'Employé' => 'ROLE_EMPLOYE',
-                    'Vétérinaire' => 'ROLE_VETERINAIRE',
-                ],
-                'expanded' => false,
-                'multiple' => true,
-            ])
-            ->add('nom', TextType::class, ['required' => true])
-            ->add('prenom', TextType::class, ['required' => true])
-            ->add('email', EmailType::class, ['required' => true])
-            ->getForm();
-    
-        $form->handleRequest($request);
-    
-        if ($form->isSubmitted() && $form->isValid()) {
-            if (!$user->getEmail()) {
-                $this->addFlash('error', 'L\'email est obligatoire.');
-                return $this->redirectToRoute('admin_user_create');
-            }
-    
-            if (!$user->getUsername()) {
-                $user->setUsername($user->getEmail());
-            }
-    
-            $user->setPassword(
-                $passwordHasher->hashPassword($user, $user->getPassword())
-            );
-    
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
-    
-            return $this->redirectToRoute('admin_user_notify', ['id' => $user->getId()]);
-        }
-    
-        return $this->render('admin/user_create.html.twig', [
-            'form' => $form->createView(),
-            'user' => $user,
-        ]);
-    }
-    
-
-    
-
-    
-
-#[Route('/edit/{id}', name: 'user_edit', methods: ['GET', 'POST'])]
-public function edit(Request $request, User $user, EntityManagerInterface $em): Response
-{
-    // pour vérifier si on tente de modifier un compte administrateur (avant de modifier quoi que ce soit)
-    if (in_array('ROLE_ADMIN', $user->getRoles())) {
-        throw $this->createAccessDeniedException('Impossible de modifier un compte Administrateur.');
-    }
-
-    // formulaire pour modifier les rôles de l'utilisateur
-    $form = $this->createFormBuilder($user)
-        ->add('username', EmailType::class, [
-            'required' => false, // Rendre ce champ optionnel
-        ])
-        ->add('password', PasswordType::class)
-        ->add('roles', ChoiceType::class, [
-            'choices' => [
-                'Employé' => 'ROLE_EMPLOYE',
-                'Vétérinaire' => 'ROLE_VETERINAIRE',
-            ],
-            'expanded' => false,
-            'multiple' => true,
-        ])
-        ->add('nom', TextType::class)
-        ->add('prenom', TextType::class)
-        ->add('email', EmailType::class)
-        ->getForm();
-
-    $form->handleRequest($request);
-
-    // Vérifie si le formulaire est soumis et valide
-    if ($form->isSubmitted() && $form->isValid()) {
-        // Si le username est vide, on le remplit avec l'email
-        if (!$user->getUsername()) {
-            $user->setUsername($user->getEmail());
-        }
-
-        // Sauvegarde les modifications dans la base de données
-        $em->persist($user);
-        $em->flush();
-
-        // Ajoute un message flash pour informer du succès de l'opération
-        $this->addFlash('success', 'Les rôles de l\'utilisateur ont été mis à jour avec succès.');
-
-        // Redirige vers la liste des utilisateurs après la mise à jour
-        return $this->redirectToRoute('user_list');
-    }
-
-    // Retourne la vue avec le formulaire et l'utilisateur
-    return $this->render('admin/user_edit.html.twig', [  // Chemin corrigé pour le template
-        'form' => $form->createView(),
-        'user' => $user,
-    ]);
-}
-
-    // Route pour afficher la liste des utilisateurs
-    #[Route('/admin/users', name: 'admin_user_list')]
-    public function listUsers(UserRepository $userRepository): Response
-    {
-        // Récupérer tous les utilisateurs depuis la base de données
-        $users = $userRepository->findAll();
-
-        // Afficher la vue avec les utilisateurs
-        return $this->render('admin/user_list.html.twig', [
-            'users' => $users,
-        ]);
-    }
-
-        
-
-    #[Route('/admin/user/notify/{id}', name: 'admin_user_notify')]
-    public function notifyUser(int $id, UserRepository $userRepository, MailerInterface $mailer): Response
-    {
-        // Récupération de l'utilisateur
-        $user = $userRepository->find($id);
-        if (!$user) {
-            throw $this->createNotFoundException('Utilisateur non trouvé.');
-        }
-    
-        // Création de l'email
-        $email = (new Email())
-            ->from('noreply@arcadia.com')
-            ->to($user->getEmail())
-            ->subject('Votre compte a été créé')
-            ->html($this->renderView('emails/notify_user.html.twig', ['user' => $user]));
-    
-        // Envoi de l'email
-        try {
-            $mailer->send($email);
-            $this->addFlash('success', 'Notification envoyée avec succès.');
-        } catch (\Exception $e) {
-            $this->addFlash('error', 'Erreur lors de l\'envoi de l\'e-mail : ' . $e->getMessage());
-        }
-    
-        return $this->redirectToRoute('admin_dashboard');
-    }
     
    
-  #[Route('/logout', name: 'app_logout')]
-   public function logout(Request $request): RedirectResponse
-   {
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(Request $request): RedirectResponse
+    {
     $session = $request->getSession();
     $session->invalidate();  
     
     return $this->redirectToRoute('app_home');
-   }
+    }
 
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,75 +1,155 @@
 <?php
 
-//namespace App\Controller;
+namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-//#[Route('/admin/user')]
-//class UserController extends AbstractController
-//{
-    // #[Route('/edit/{id}', name: 'user_edit', methods: ['GET', 'POST'])]
-    // public function edit(Request $request, User $user, EntityManagerInterface $em): Response
-    // {
-    //     // Vérifie si on tente de modifier un compte administrateur
-    //     if (in_array('ROLE_ADMIN', $user->getRoles())) {
-    //         throw $this->createAccessDeniedException('Impossible de modifier un compte Administrateur.');
-    //     }
+#[Route('/admin/user')]
+class UserController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
 
-    //     // Crée le formulaire pour modifier les rôles de l'utilisateur
-    //     $form = $this->createFormBuilder($user)
-    //         ->add('roles', ChoiceType::class, [
-    //             'choices' => [
-    //                 'Employé' => 'ROLE_EMPLOYE',
-    //                 'Vétérinaire' => 'ROLE_VETERINAIRE',
-    //             ],
-    //             'expanded' => false,
-    //             'multiple' => true,
-    //             'label' => 'Rôles',
-    //         ])
-    //         ->getForm();
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
-    //     $form->handleRequest($request);
+    #[Route('/create', name: 'admin_user_create')]
+    public function createUser(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
 
-    //     // Vérifie si le formulaire est soumis et valide
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         // Vérifie à nouveau si le rôle admin est assigné dans les données soumises
-    //         if (in_array('ROLE_ADMIN', $user->getRoles())) {
-    //             throw $this->createAccessDeniedException('Impossible d\'attribuer le rôle Administrateur.');
-    //         }
+        $form = $this->createFormBuilder($user)
+            ->add('username', EmailType::class, ['required' => false])
+            ->add('password', PasswordType::class)
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Employé' => 'ROLE_EMPLOYE',
+                    'Vétérinaire' => 'ROLE_VETERINAIRE',
+                ],
+                'expanded' => false,
+                'multiple' => true,
+            ])
+            ->add('nom', TextType::class, ['required' => true])
+            ->add('prenom', TextType::class, ['required' => true])
+            ->add('email', EmailType::class, ['required' => true])
+            ->getForm();
 
-    //         $em->persist($user);
-    //         $em->flush();
+        $form->handleRequest($request);
 
-    //         $this->addFlash('success', 'Les rôles de l\'utilisateur ont été mis à jour avec succès.');
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$user->getEmail()) {
+                $this->addFlash('error', 'L\'email est obligatoire.');
+                return $this->redirectToRoute('admin_user_create');
+            }
 
-    //         return $this->redirectToRoute('user_list');
-    //     }
+            if (!$user->getUsername()) {
+                $user->setUsername($user->getEmail());
+            }
 
-    //     return $this->render('admin/user/edit.html.twig', [
-    //         'form' => $form->createView(),
-    //         'user' => $user,
-    //     ]);
-    // }
+            $user->setPassword(
+                $passwordHasher->hashPassword($user, $user->getPassword())
+            );
 
-    // #[Route('/', name: 'user_list', methods: ['GET'])]
-    // public function index(EntityManagerInterface $em): Response
-    // {
-    //     // Récupération de la liste des utilisateurs
-    //     $users = $em->getRepository(User::class)->findAll();
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
 
-    //     return $this->render('admin/user/index.html.twig', [
-    //         'users' => $users,
-    //     ]);
-    // }
-    
-    // public function eraseCredentials(): void
-    // {
-    //     // Logique éventuelle pour supprimer les informations sensibles
-    // }
-//}
+            return $this->redirectToRoute('admin_user_notify', ['id' => $user->getId()]);
+        }
+
+        return $this->render('admin/user_create.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $em): Response
+    {
+        if (in_array('ROLE_ADMIN', $user->getRoles())) {
+            throw $this->createAccessDeniedException('Impossible de modifier un compte Administrateur.');
+        }
+
+        $form = $this->createFormBuilder($user)
+            ->add('username', EmailType::class, ['required' => false])
+            ->add('password', PasswordType::class)
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Employé' => 'ROLE_EMPLOYE',
+                    'Vétérinaire' => 'ROLE_VETERINAIRE',
+                ],
+                'expanded' => false,
+                'multiple' => true,
+            ])
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('email', EmailType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$user->getUsername()) {
+                $user->setUsername($user->getEmail());
+            }
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Les rôles de l\'utilisateur ont été mis à jour avec succès.');
+            return $this->redirectToRoute('admin_user_list');
+        }
+
+        return $this->render('admin/user_edit.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/list', name: 'admin_user_list')]
+    public function listUsers(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render('admin/user_list.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    #[Route('/notify/{id}', name: 'admin_user_notify')]
+    public function notifyUser(int $id, UserRepository $userRepository, MailerInterface $mailer): Response
+    {
+        $user = $userRepository->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non trouvé.');
+        }
+
+        $email = (new Email())
+            ->from('noreply@arcadia.com')
+            ->to($user->getEmail())
+            ->subject('Votre compte a été créé')
+            ->html($this->renderView('emails/notify_user.html.twig', ['user' => $user]));
+
+        try {
+            $mailer->send($email);
+            $this->addFlash('success', 'Notification envoyée avec succès.');
+        } catch (\Exception $e) {
+            $this->addFlash('error', 'Erreur lors de l\'envoi de l\'e-mail : ' . $e->getMessage());
+        }
+
+        return $this->redirectToRoute('admin_user_list');
+    }
+}
