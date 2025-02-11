f33140f3bd509f38d4d17930919562b9855988b1
Tchoubi11 <tchoubayefranck@gmail.com>
2025-02-11 20:43:25 +0100

    Mise à jour des fichiers routes.yaml,dashboardController.php,dashboard.html.twig,création du template admin/_views.html et suppréssion du template admin_dashboard.html.twig et adminController.php

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,6 +6,10 @@ use App\Document\AnimalDocument;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
 
 class DashboardController extends AbstractController
 {
@@ -21,4 +25,41 @@ class DashboardController extends AbstractController
 
     return new JsonResponse($statistics);
 }
+#[Route('/animalDocument/getViews/{prenom}', name: 'animal_get_views', methods: ['GET'])]
+public function getViewsByPrenom(string $prenom, DocumentManager $dm, Environment $twig): JsonResponse
+{
+    $animal = $dm->getRepository(AnimalDocument::class)->findOneBy(['prenom' => $prenom]);
+
+    if (!$animal) {
+        return new JsonResponse(['error' => 'Animal non trouvé'], Response::HTTP_NOT_FOUND);
+    }
+
+    $views = $animal->getViews();
+
+    // Génération de l'HTML sans l'échapper
+    $html = $twig->render('admin/_views.html.twig', [
+        'views' => $views
+    ]);
+
+    return new JsonResponse([
+        'views' => $views, 
+        'html' => html_entity_decode($html) // Décode les entités HTML
+    ]);
+}
+
+
+#[Route('/admin', name: 'admin_dashboard')]
+public function dashboard(DocumentManager $dm): Response
+{
+    // Récupération de tous les animaux depuis MongoDB
+    $animals = $dm->getRepository(AnimalDocument::class)->findAll();
+    dump($animals);
+
+    error_log("Nombre d'animaux trouvés : " . count($animals));
+
+    return $this->render('admin/dashboard.html.twig', [
+        'animals' => $animals, // Passe les animaux au template
+    ]);
+}
+
 }
